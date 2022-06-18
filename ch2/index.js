fee1c200833882104698b65c7c67daf68ac78a89
birendra20 <birendragupta20@gmail.com>
2022-06-18 20:00:08 +0530

    wrapper module

---
+++ ch2/index.js
@@ -0,0 +1,10 @@
+//module wrapper
+
+//before a modules code is executed ,node.js will wrap it with function wrapper that look like the following
+
+// (function(exports,require,module,__filename,__dirname)){
+//     //module code actually lives in here
+// }
+
+//console.log(__dirname);
+//  console.log(__filename);
