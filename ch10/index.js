2f417135bea19bc3192890dacfb0778e4eb7efb2
birendra20 <birendragupta20@gmail.com>
2022-06-19 17:22:43 +0530

    http module

--- ch10/index.js
+++ ch10/index.js
@@ -1,10 +1,20 @@
-//nodemon
+//nodemon and http
 
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.setHeader("content-type", "text/plain");
-  res.end("response from server 2");
+  //Request
+  // console.log(req.url);
+  // if (req.url != "/favicon.ico") {
+  //   console.log(req.url);
+  // }
+  // console.log(req.method);
+  //Response
+  //res.statusCode =202;
+  //res.status.Message="Good";
+  //res.setHeader("content-type", "text/plain");
+  //res.writeHead(202,"Good",{"content-type":"text/plain"})
+  //res.end("response from server 2");
 });
 
 const PORT = process.env.PORT || 8000;
